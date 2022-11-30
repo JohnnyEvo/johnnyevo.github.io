410c84f0935fc5cfad377a886c88c00992ea81e8
Johnny Charcosset <j.charcosset@onlineformapro.com>
2022-11-30 13:06:40 +0100

    feat: image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,12 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://johnnyevo.github.io/',
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
