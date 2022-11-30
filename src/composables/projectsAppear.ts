d314136637f69f9afa58180d96e9ca5e5f1e7947
Johnny Charcosset <j.charcosset@onlineformapro.com>
2022-11-30 14:55:40 +0100

    feat: fix mobile

--- src/composables/projectsAppear.ts
+++ src/composables/projectsAppear.ts
@@ -1,31 +1,35 @@
 import { animate } from "motion";
 
 export const useProjectsAppear = async () => {
-    const leftElements = Array.from(
-        (document.getElementById("left-side") as HTMLElement)
-            .children as HTMLCollectionOf<HTMLElement>
-    );
-    const rightElements: Element[] = Array.from(
-        (document.getElementById("right-side") as HTMLElement)
-            .children as HTMLCollectionOf<HTMLElement>
-    );
+    const isMobile = window.matchMedia("only screen and (max-width: 768px)").matches;
 
-    let elements: HTMLElement[] = [];
+    if (!isMobile) {
+        const leftElements = Array.from(
+            (document.getElementById("left-side") as HTMLElement)
+                .children as HTMLCollectionOf<HTMLElement>
+        );
+        const rightElements: Element[] = Array.from(
+            (document.getElementById("right-side") as HTMLElement)
+                .children as HTMLCollectionOf<HTMLElement>
+        );
 
-    leftElements.forEach((leftElement, index) => {
-        elements.push(leftElement);
-        leftElement.style.opacity = "0";
-        if (index in rightElements) {
-            elements.push(rightElements[index] as HTMLElement);
-            (rightElements[index] as HTMLElement).style.opacity = "0";
+        let elements: HTMLElement[] = [];
+
+        leftElements.forEach((leftElement, index) => {
+            elements.push(leftElement);
+            leftElement.style.opacity = "0";
+            if (index in rightElements) {
+                elements.push(rightElements[index] as HTMLElement);
+                (rightElements[index] as HTMLElement).style.opacity = "0";
+            }
+        });
+
+        for (const element of elements) {
+            await animate(
+                element,
+                { opacity: 1, transform: "translateY(0)" },
+                { duration: 0.7 }
+            ).finished;
         }
-    });
-
-    for (const element of elements) {
-        await animate(
-            element,
-            { opacity: 1, transform: "translateY(0)" },
-            { duration: 0.7 }
-        ).finished;
     }
 }
