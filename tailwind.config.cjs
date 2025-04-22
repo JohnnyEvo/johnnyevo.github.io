0cd0aa566436b9cd8629327b2af892b97f17fabb
Johnny Charcosset <johnny.charcosset@steeple.com>
2025-04-22 09:26:38 +0200

    feat: add psychology site

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        extend: {
-            fontFamily: {
-                'title': ['Chillax'],
-                'sans': ['Synonym'],
-            },
-            colors: {
-                primary: 'rgb(var(--color-primary))',
-                secondary: 'rgb(var(--color-secondary))',
-                alternative: 'rgb(var(--color-alternative))',
-            },
-        }
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [require("tailwind-scrollbar")],
+  theme: {
+    extend: {
+      fontFamily: {
+        title: ["Chillax"],
+        sans: ["Synonym"],
+      },
+      colors: {
+        primary: "rgb(var(--color-primary))",
+        secondary: "rgb(var(--color-secondary))",
+        alternative: "rgb(var(--color-alternative))",
+      },
     },
-}
+  },
+};
